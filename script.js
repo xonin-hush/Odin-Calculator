3d52cff015d717b0998525f649dbd1e3760153e1
xonin-hush <xonindev@gmail.com>
2023-07-29 16:54:43 +0100

    Create basic mathematical functions, add,sub,mult,div

---
+++ script.js
@@ -0,0 +1,18 @@
+let firstNumber = undefined
+let operator = undefined
+let secondNumber = undefined
+
+
+
+function addition(a, b) {
+    sum = a + b
+}
+function subtraction(a, b) {
+    sub = a - b
+}
+function division(a, b) {
+    product = a / b
+}
+function multiplication(a, b) {
+    product = a * b
+}
