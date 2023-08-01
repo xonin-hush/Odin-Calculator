e161f91f0f2d1671ab1c3fd55d7c0db2819704f8
xonin-hush <xonindev@gmail.com>
2023-08-01 14:34:21 +0100

    Make = button operate by converting strings to intgers

--- script.js
+++ script.js
@@ -1,43 +1,55 @@
 let firstNumber = ""
 let operator = undefined
 let secondNumber = ""
-
+let finalValue = undefined
 const numContainer = document.querySelector('.digit-buttons');
-const oppContainer=document.querySelector('.function-buttons')
-
+const oppContainer = document.querySelector('.operator-buttons')
+const equal = document.querySelector('#equal')
 numContainer.addEventListener('click', function (e) {
-    if (!operator)
+    if (!operator) {
         firstNumber = firstNumber + e.target.innerHTML
-    else secondNumber = secondNumber + e.target.innerHTML
+        firstNumber = parseInt(firstNumber)
+    }
+    else {
+        secondNumber = secondNumber + e.target.innerHTML
+        secondNumber = parseInt(secondNumber)
+    }
 });
 oppContainer.addEventListener('click', function (e) {
-    operator=e.target.innerHTML
+    operator = e.target.innerHTML
+});
+
+equal.addEventListener('click', () => {
+    if ((firstNumber && secondNumber) != (""))
+
+        operate(firstNumber, operator, secondNumber)
 });
 function addition(a, b) {
-    sum = a + b
+    return sum = a + b
 }
 function subtraction(a, b) {
-    sub = a - b
+    return sub = a - b
 }
 function division(a, b) {
-    product = a / b
+    return product = a / b
 }
 function multiplication(a, b) {
-    product = a * b
+    return product = a * b
 }
 function operate(firstNum, opp, secondNum) {
     if (opp == "+")
-        addition(firstNum, secondNum)
+        console.log(addition(firstNum, secondNum))
     if (opp == "-")
-        subtraction(firstNum, secondNum)
+        finalValue = subtraction(firstNum, secondNum)
     if (opp == "/")
-        division(firstNum, secondNum)
+        finalValue = division(firstNum, secondNum)
     if (opp == "*")
-        multiplication(firstNum, secondNum)
+        finalValue = multiplication(firstNum, secondNum)
 
 }
 function doStuff() {
-    console.log("firstNumber " + firstNumber);
+    console.log("firstNumber " + firstNumber );
     console.log("secondNumber " + secondNumber);
+    console.log("finalValue " + finalValue)
 }
 setInterval(doStuff, 5000);
