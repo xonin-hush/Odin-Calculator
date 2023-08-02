1ce4d7a0fe9c1be529217e15f00bc477e4fae28f
xonin-hush <xonindev@gmail.com>
2023-08-02 20:15:34 +0100

    Create handleDisplay function to display values

--- script.js
+++ script.js
@@ -5,9 +5,27 @@ let finalValue = undefined
 const numContainer = document.querySelector('.digit-buttons');
 const oppContainer = document.querySelector('.operator-buttons')
 const equal = document.querySelector('#equal')
+const body = document.querySelector('body')
+const display = document.querySelector('.display')
 handleNumbers()
 handleOperators()
 handleEqual()
+handleDisplay()
+
+function handleDisplay() {
+    body.addEventListener('click', () => {
+        if ((operator === undefined) && (secondNumber === "") && (finalValue === undefined)) {
+            display.innerHTML = firstNumber
+        }
+        if ((operator !== undefined) && (firstNumber !== "") && (secondNumber === "")) {
+            display.innerHTML = operator
+        }
+        if ((operator !== undefined) && (firstNumber !== "") && (secondNumber !== "")) {
+            display.innerHTML = secondNumber
+        }
+    })
+}
+
 function handleNumbers() {
     numContainer.addEventListener('click', function (e) {
         if ((!operator)) {
@@ -20,23 +38,26 @@ function handleNumbers() {
         }
     });
 }
-function handleOperators(){
-oppContainer.addEventListener('click', function (e) {
-    if ((!operator) && (firstNumber !== "")) {
-        operator = e.target.innerHTML
-    }
-    if ((firstNumber !== "") && (secondNumber !== "") && (operator !== undefined)) {
-        operate(firstNumber, operator, secondNumber)
-        operator = e.target.innerHTML
-    }
-});}
-function handleEqual(){
-equal.addEventListener('click', () => {
-    if ((firstNumber !== "") && (secondNumber !== "")) {
-        operate(firstNumber, operator, secondNumber)
-        operator = undefined;
-    }
-});}
+function handleOperators() {
+    oppContainer.addEventListener('click', function (e) {
+        if ((!operator) && (firstNumber !== "")) {
+            operator = e.target.innerHTML
+        }
+        if ((firstNumber !== "") && (secondNumber !== "") && (operator !== undefined)) {
+            operate(firstNumber, operator, secondNumber)
+            operator = e.target.innerHTML
+        }
+    });
+}
+function handleEqual() {
+    equal.addEventListener('click', () => {
+        if ((firstNumber !== "") && (secondNumber !== "")) {
+            operate(firstNumber, operator, secondNumber)
+            operator = undefined;
+            display.innerHTML = finalValue
+        }
+    });
+}
 
 function addition(a, b) {
     return sum = a + b;
