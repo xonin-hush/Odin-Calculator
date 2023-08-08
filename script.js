eb65dcc4928b48ed3efbc2536fbbfb4b86595331
xonin-hush <xonindev@gmail.com>
2023-08-08 18:19:24 +0100

    Fix displaying all digit-buttons when clicked operator-buttons

--- script.js
+++ script.js
@@ -8,41 +8,61 @@ const equal = document.querySelector('#equal')
 const body = document.querySelector('body')
 const display = document.querySelector('.display')
 const button = document.querySelector('.button')
+const operatorNodeList = document.querySelectorAll('.operator-buttons')
 handleNumbers()
 handleOperators()
 handleEqual()
 handleDisplay()
+// displayCurrentOperator()
 function handleDisplay() {
     body.addEventListener('click', () => {
         if ((operator === undefined) && (secondNumber === "") && (finalValue === undefined)) {
             display.innerHTML = firstNumber
-
-        }
-        if ((operator !== undefined) && (firstNumber !== "") && (secondNumber === "")) {
-            display.innerHTML = operator
         }
         if ((operator !== undefined) && (firstNumber !== "") && (secondNumber !== "")) {
             display.innerHTML = secondNumber
         }
         if ((display.innerHTML !== undefined) && (display.innerHTML !== "")) {
             display.style.paddingTop = `20px`
-            console.log('hi')
         }
+        if ((operator !== undefined) && (firstNumber == finalValue) && (secondNumber === "") && (firstNumber !== ""))
+            display.innerHTML = firstNumber
     })
 }
+// function displayCurrentOperator() {
+//     bruh=operatorNodeList[1]
+//     oppContainer.addEventListener('click', ()=> {
+//         bruh.style.backgroundColor='white'
 
+//         console.log(bruh)
+//         if (operator === "+") {
+
+//         }
+//         if (operator === "-") {
+
+//         }
+//         if (operator === "*") {
+
+//         }
+//         if (operator === "/") {
+
+//         }
+//     });
+// }
 function handleNumbers() {
+    
     numContainer.addEventListener('click', function (e) {
-
+        let checkButtons=e.target.innerHTML
+        console.log(checkButtons)
+        if (checkButtons.includes("button"))
+            return;
         if (e.target.innerHTML === ".")
             return;
         if ((!operator)) {
             firstNumber = firstNumber + e.target.innerHTML
-            // firstNumber = Number(firstNumber)
         }
         else {
             secondNumber = secondNumber + e.target.innerHTML
-            //secondNumber = Number(secondNumber)
         }
     });
 }
@@ -71,6 +91,7 @@ function handleOperators() {
     oppContainer.addEventListener('click', function (e) {
         if ((!operator) && (firstNumber !== "")) {
             operator = e.target.innerHTML
+            console.log(e.target.innerHTML)
         }
         if ((firstNumber !== "") && (secondNumber !== "") && (operator !== undefined)) {
             firstNumber = Number(firstNumber)
@@ -116,24 +137,24 @@ function operate(firstNum, opp, secondNum) {
 
     if (opp === "+") {
         finalValue = (addition(firstNum, secondNum))
-        firstNumber = finalValue
+        firstNumber = finalValue.toString()
         secondNumber = ""
 
     }
     if (opp === "-") {
         finalValue = subtraction(firstNum, secondNum)
-        firstNumber = finalValue
+        firstNumber = finalValue.toString()
         secondNumber = ""
 
     }
     if (opp === "/") {
         finalValue = division(firstNum, secondNum)
-        firstNumber = finalValue
+        firstNumber = finalValue.toString()
         secondNumber = ""
     }
     if (opp === "*") {
         finalValue = multiplication(firstNum, secondNum)
-        firstNumber = finalValue
+        firstNumber = finalValue.toString()
         secondNumber = ""
 
     }
@@ -151,6 +172,5 @@ function reset() {
 //     console.log("secondNumber " + secondNumber);
 //     console.log("finalValue " + finalValue);
 //     console.log("operator " + operator);
-//     console.log({numContainer})
 // }
-// setInterval(doStuff, 3000);
+// setInterval(doStuff, 3000)
